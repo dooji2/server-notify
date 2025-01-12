f7be3e46409a9217f9a7e468de3d2dded8d4f9e8
dooji2 <doojisbasement@gmail.com>
2025-01-12 14:12:20 +0200

    Fix commands

--- src/main/java/com/dooji/sn/commands/NotificationCommands.java
+++ src/main/java/com/dooji/sn/commands/NotificationCommands.java
@@ -211,11 +211,11 @@ public class NotificationCommands {
             case "texture":
             case "width":
             case "height":
-            case "dismissMessage":
+            case "dismissmessage":
             case "alwaysshow":
             case "message":
             case "url":
-            case "dismissButton":
+            case "dismissbutton":
                 editNotification(source, uuid, whatToEdit, newValue);
                 return 1;
             default:
@@ -461,7 +461,7 @@ public class NotificationCommands {
                     case "height":
                         notification.addProperty("height", Integer.parseInt(newValue));
                         break;
-                    case "dismissMessage":
+                    case "dismissmessage":
                         notification.addProperty("dismissMessage", Boolean.parseBoolean(newValue));
                         break;
                     case "alwaysshow":
@@ -473,7 +473,7 @@ public class NotificationCommands {
                     case "url":
                         notification.addProperty("url", newValue);
                         break;
-                    case "dismissButton":
+                    case "dismissbutton":
                         notification.addProperty("dismissButton", Boolean.parseBoolean(newValue));
                         break;
                     default:
